38b5a222b1f9aabea5d19321b958ec7e2c48251a
joaopa24 <joaokn129@gmail.com>
2022-02-11 01:48:48 -0300

    input and emoji

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading'
 
-import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 import {
   useFonts,
   Jost_400Regular,
@@ -17,7 +17,7 @@ export default function App() {
   if(!fontsLoaded) return <AppLoading/> 
 
   return (
-     <Welcome />
+     <UserIdentification />
   )
 }
 
