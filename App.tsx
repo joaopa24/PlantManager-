b50732fbde84858443639f846975c78333ed51f5
joaopa24 <joaokn129@gmail.com>
2022-03-19 10:23:01 -0300

    confirmation and user identification pages

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading'
 
-import { UserIdentification } from './src/pages/UserIdentification';
+import { Confirmation } from './src/pages/Confirmation';
 import {
   useFonts,
   Jost_400Regular,
@@ -17,7 +17,7 @@ export default function App() {
   if(!fontsLoaded) return <AppLoading/> 
 
   return (
-     <UserIdentification />
+     <Confirmation />
   )
 }
 
