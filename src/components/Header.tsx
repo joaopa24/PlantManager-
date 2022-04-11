27b00b4ec02f383c77ac390fec418c4911f79b9d
joaopa24 <joaokn129@gmail.com>
2022-04-10 22:59:44 -0300

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+import colors from "../styles/colors";
+import userimg from "../assets/photouser.png"
+import fonts from "../styles/fonts";
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>João</Text>
+      </View>
+
+      <Image source={userimg} style={styles.image}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+  },
+  image:{
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40
+  }
+
+});
---
+++ src/pages/PlantSelect.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import colors from "../styles/colors";
+
+import { Header } from "../components/Header";
+import fonts from "../styles/fonts";
+
+export function PlantSelect() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  header:{
+    paddingHorizontal:30
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+});
